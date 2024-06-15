5cdba08398c2e5d2dd44793781db74ed4f689102
Andrey Danilov <dndred@gmail.com>
2024-06-15 14:58:19 +0300

    add getArgumentsCountInLimit with tests

--- packages/firebird-ast-sqlify/src/limit.test.ts
+++ packages/firebird-ast-sqlify/src/limit.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'bun:test'
-import { Limit, limitSchema, sqlifyLimit } from './limit'
+import {getArgumentsCountInLimit, Limit, limitSchema, sqlifyLimit} from './limit'
 
 const limitExample: Limit = {
   seperator: '',
@@ -25,6 +25,31 @@ const limitWithOffsetExample: Limit = {
   ],
 } as const
 
+
+const limitWithArgumentExample: Limit = {
+  "seperator": "",
+  "value": [
+    {
+      "type": "origin",
+      "value": "?"
+    }
+  ]
+}
+
+const limitWithArgumentAndOffset: Limit = {
+    "seperator": "offset",
+    "value": [
+      {
+        "type": "origin",
+        "value": "?"
+      },
+      {
+        "type": "origin",
+        "value": "?"
+      }
+    ]
+  }
+
 describe('limitSchema', () => {
   it('should validate limit', () => {
     const valid = limitSchema.safeParse(limitExample)
@@ -33,6 +58,16 @@ describe('limitSchema', () => {
 
   it('should validate limit with offset', () => {
     const valid = limitSchema.safeParse(limitWithOffsetExample)
+    expect(valid.success).toBe(true)
+  })
+
+  it('should validate limit with argument', () => {
+    const valid = limitSchema.safeParse(limitWithArgumentExample)
+    expect(valid.success).toBe(true)
+  })
+
+  it('should validate limit and offset with argument', () => {
+    const valid = limitSchema.safeParse(limitWithArgumentAndOffset)
     expect(valid.success).toBe(true)
   })
 })
@@ -44,5 +79,19 @@ describe('sqlifyLimit', () => {
 
   it('should convert limit with offset', () => {
     expect(sqlifyLimit(limitWithOffsetExample)).toBe('first 10 skip 5')
+  })
+})
+
+describe('getArgumentsCountInLimit', () => {
+  it('limit without argument', () => {
+    expect(getArgumentsCountInLimit(limitWithOffsetExample)).toBe(0)
+  })
+
+  it('limit with argument', () => {
+    expect(getArgumentsCountInLimit(limitWithArgumentExample)).toBe(1)
+  })
+
+  it('limit with offset and arguments', () => {
+    expect(getArgumentsCountInLimit(limitWithArgumentAndOffset)).toBe(2)
   })
 })
